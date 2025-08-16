aad797fe3dd47c83626e51d421b3307c5943ccb7
orcnog <orcnog@gmail.com>
2025-08-16 09:44:20 -0500

    update controller path to 2014.5e.orcnog.com. i guess?

--- js/controller-config.js
+++ js/controller-config.js
@@ -1,2 +1,2 @@
 export const VOICE_APP_PATH = "https://d20.orcnog.com/";
-export const CONTROLLER_APP_PATH = "https://5e.orcnog.com/";
+export const CONTROLLER_APP_PATH = "https://2014.5e.orcnog.com/";
